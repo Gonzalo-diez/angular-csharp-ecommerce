fb06642ccce32621e2058b809d74d4368166eec6
Gonzalo <gonzalodiez97@gmail.com>
2025-03-27 16:12:38 -0300

    Agregado de boton para eliminar productos

--- Frontend/src/app/features/product/product-shared/product-shared.component.ts
+++ Frontend/src/app/features/product/product-shared/product-shared.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProductModel } from '../../../core/models/product/product.model';
+import { AuthRole } from '../../../core/models/auth/auth.role';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { ProductService } from '../../../core/services/product/product.service';
 
 @Component({
   selector: 'app-product-shared',
@@ -9,6 +12,69 @@ import { ProductModel } from '../../../core/models/product/product.model';
   templateUrl: './product-shared.component.html',
   styleUrl: './product-shared.component.css'
 })
-export class ProductSharedComponent {
+export class ProductSharedComponent implements OnInit {
+  userId: number | null = null;
+  userRole: string | null = null;
+
+  constructor(private authService: AuthService, private productService: ProductService) {}
+
   @Input() products: ProductModel[] = [];
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated().subscribe((isAuth) => {
+      if (isAuth) {
+        const token = this.authService.getToken();
+        if (token) {
+          this.userId = this.decodeToken(token);
+        }
+      }
+    });
+  }
+
+  isAuthenticated() {
+    this.authService.isAuthenticated();
+  }
+
+  deleteProduct(productId: number, ownerId: number) {
+    if (productId === undefined) {
+      console.warn('⚠️ Producto sin ID, no se puede eliminar.');
+      return;
+    }
+
+    if (this.userId !== ownerId && this.userRole !== AuthRole.Admin) {
+      console.warn('You dont have the permission to delete this product');
+      return;
+    }
+
+    this.productService.deleteProduct(productId, Number(this.userId)).subscribe({
+      next: () => {
+        console.log('✅ Producto eliminado con éxito.');
+        this.products = this.products.filter(product => product.id !== productId);
+      },
+      error: (err) => console.error('❌ Error deleting product:', err)
+    })
+  }
+
+  private decodeToken(token: string): number | null {
+    try {
+      const base64Url = token.split('.')[1];
+      if (!base64Url) {
+        console.error('Formato de token inválido');
+        return null;
+      }
+
+      const payload = JSON.parse(atob(base64Url));
+
+      // Extraer userId correctamente
+      if (payload.user) {
+        const userObject = JSON.parse(payload.user);
+        return userObject.Id || null;
+      }
+
+      return null;
+    } catch (e) {
+      console.error('Error al decodificar el token:', e);
+      return null;
+    }
+  }
 }
